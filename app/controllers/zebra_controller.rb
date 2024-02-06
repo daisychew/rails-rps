5e7742bdf1c5f36d8e8b79d0021f3bfeeb75e1df
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-06 22:00:15 +0000

    finished up until part of rock

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,19 @@
+class ZebraController < ApplicationController
+  def rock
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def paper
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def scissors
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_scissors"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "zebra", :action => "rock"})
 
+  get("/paper", { :controller => "zebra", :action => "paper"})
+
+  get("/scissors", { :controller => "zebra", :action => "scissors"})
 end
